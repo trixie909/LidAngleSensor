f78027e498efe3cfe6a76c962d0fbc438b5e7905
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2025-09-06 13:07:46 -0400

    Initial Commit

---
+++ LidAngleSensor/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LidAngleSensor
+//
+//  Created by Sam on 2025-09-06.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LidAngleSensor/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  LidAngleSensor
+//
+//  Created by Sam on 2025-09-06.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
